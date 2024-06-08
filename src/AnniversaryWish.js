4fce9e42ab8c1312427e32423521dab6a7a6d165
ThaminduSathruwan <sathruwan99@gmail.com>
2024-06-08 23:53:44 +0530

    2nd

--- src/AnniversaryWish.js
+++ src/AnniversaryWish.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import SpecialMessage from "./SpecialMessage";
+import "./AnniversaryWish.css";
 
 const AnniversaryWish = () => {
   const [showMessage, setShowMessage] = useState(false);
@@ -9,14 +10,19 @@ const AnniversaryWish = () => {
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center vh-100">
-      <Row>
-        <Col>
+    <Container className="d-flex justify-content-center align-items-center min-vh-100">
+      <Row className="w-100 mx-0">
+        <Col xs={12} md={8} lg={6} className="mx-auto">
           <Card className="text-center anniversary-card">
             <Card.Body>
-              <Card.Title className="display-4 title ">
-                <span className="highlight">Happy Anniversary, My Love!</span>
+              <Card.Title className="display-4 title">
+                Happy Anniversary, My Love!
               </Card.Title>
+              <Card.Text className="lead">
+                To the most amazing girlfriend in the world, happy anniversary!
+                You make my life complete, and I can't wait to spend many more
+                anniversaries with you.
+              </Card.Text>
               <Button
                 variant={showMessage ? "secondary" : "danger"}
                 onClick={handleClick}
